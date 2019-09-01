abf405afadc9976bb9c6b42a0b4fb9d0a4352462
Stefan Guggisberg <sguggisb@adobe.com>
2019-09-01 14:24:04 +0200

    fix(index): log error

--- src/index.js
+++ src/index.js
@@ -378,6 +378,7 @@ function deliverPlain(owner, repo, ref, entry, root, esi = false, branch) {
     if (rqerror.statusCode === 404 || rqerror.statusCode === '404') {
       return error(entry, rqerror.statusCode);
     }
+    log.error('error while fetching content', rqerror);
     return error(
       (rqerror.response && rqerror.response.body && rqerror.response.body.toString())
       || rqerror.message,
