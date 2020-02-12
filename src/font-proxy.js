ee14f4e8904c3a79a3f42a1a7e3ac01d96a8a87c
Lars Trieloff <trieloff@adobe.com>
2020-02-12 16:42:36 +0000

    fix(fonts): do pass push (preload) headers onto client
    This reverts commit 565cb50ac54129be25187e996ee27d0713ea27e0.
    
    see #191

--- src/font-proxy.js
+++ src/font-proxy.js
@@ -48,7 +48,7 @@ async function deliverFontCSS(file) {
         'cache-control': headers['cache-control'],
         'content-type': headers['content-type'],
         'surrogate-control': 'max-age=300, stale-while-revalidate=2592000',
-        link: (await getFontURLs(body)).map((url) => `<${url}>; rel=preload; as=font; x-http2-push-only`).join(','),
+        link: (await getFontURLs(body)).map((url) => `<${url}>; rel=preload; as=font`).join(','),
       },
       body: body.replace(/https:\/\/use\.typekit\.net\//g, '/hlx_fonts/'),
     };
