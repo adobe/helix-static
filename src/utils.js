e90f82326e04f7983c647e84c627533024c87344
Stefan Guggisberg <sguggisb@adobe.com>
2021-02-11 09:22:22 +0100

    fix(deps): migrate to helix-fetch v2 (#496)
    * fix(deps): migrate to helix-fetch v2
    
    * test: turn off recording

--- src/utils.js
+++ src/utils.js
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-const { Response } = require('node-fetch');
+const { Response } = require('@adobe/helix-fetch');
 const sanitizer = require('sanitizer');
 const log = require('@adobe/helix-log');
 
