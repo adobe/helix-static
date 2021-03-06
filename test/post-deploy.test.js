9a072940bb5204e839e896bdf55e010abfe13b84
Lars Trieloff <trieloff@adobe.com>
2021-03-06 13:51:15 +0100

    test(post-deploy): retry icons test three times

--- test/post-deploy.test.js
+++ test/post-deploy.test.js
@@ -77,7 +77,8 @@ createTargets().forEach((target) => {
         });
     }).timeout(35000);
 
-    it(`pages/icons.svg gets delivered from ${target.host()}${target.urlPath()}?owner=adobe&repo=pages&ref=d7acb4e41cf9546a40c7d6cd5e7162f8bcd540fd&path=/icons.svg`, async () => {
+    it(`pages/icons.svg gets delivered from ${target.host()}${target.urlPath()}?owner=adobe&repo=pages&ref=d7acb4e41cf9546a40c7d6cd5e7162f8bcd540fd&path=/icons.svg`, async function test() {
+      this.retries(3); // cheating, I know
       let url;
       await chai
         .request(target.host())
